10cedb7f669a23e758a77c2296171118200cb120
wuyudaxia <www@WB-WUYUDAXIA.localdomain>
2026-04-27 17:09:36 -0700

    fixed the bug code

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
-
-        result = num1 + num2;
-
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            return false;
+        }
+        const result = num1 + num2;
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -25,11 +25,16 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
+    if (!Array.isArray(prices) || typeof discount !== 'number') {
+        return false;
+    }
+    if (prices.length === 0) {
+        return false;
+    }
+    const discounted = [];
     const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+    for (let i = 0; i < length; i++) {
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
